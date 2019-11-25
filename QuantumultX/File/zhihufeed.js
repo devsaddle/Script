f7fa4eb99e92ea649e4771ac8868532894f2c7fd
Saddle <devsaddle@gmail.com>
2019-11-25 16:53:29 +0800

    Update zhihufeed.js

--- QuantumultX/File/zhihufeed.js
+++ QuantumultX/File/zhihufeed.js
@@ -4,8 +4,8 @@
 
 let body = $response.body
 body=JSON.parse(body)
-body['data'].forEach((element, index)=> {
-    if(element['card_type']=='slot_event_card'||element.hasOwnProperty('ad')){      
+body['data'].forEach((element, index)=>{
+     if(element.hasOwnProperty('ad')){      
        body['data'].splice(index,1)  
     }
 })
